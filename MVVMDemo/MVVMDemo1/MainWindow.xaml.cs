db25e6181870b4366a1ec9420496643224926d2a
rtenet <rtenet@gmail.com>
2019-07-04 18:04:02 +0300

    MVVMDemo version 1.0

---
+++ MVVMDemo/MVVMDemo1/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using MVVMDemo.ViewModel;
+
+namespace MVVMDemo1
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StudentViewControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            var obj = new StudentViewModel();
+            var type = obj.GetType();
+            var method = type.GetMethod("Print");
+            method.Invoke(obj, null);
+            StudentViewModel studentViewModelObject = new StudentViewModel();
+            studentViewModelObject.LoadStudents();
+
+            StudentViewControl.DataContext = studentViewModelObject;
+        }
+    }
+}
---
+++ MVVMDemo/MVVMDemo1/ViewModel/StudentViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using MVVMDemo.Model;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace MVVMDemo.ViewModel
+{
+
+    public class StudentViewModel
+    {
+
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+
+        public void LoadStudents()
+        {
+
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+            Student s1 = new Student();
+            //s1.PropertyChanged += s1_PropertyChanged;
+            s1.FirstName = "Mark";
+            s1.LastName = "Allain";
+            students.Add(s1);
+            students.Add(new Student { FirstName = "Allen", LastName = "Brown" });
+            students.Add(new Student { FirstName = "Linda", LastName = "Hamerski" });
+
+            Students = students;
+        }
+
+        void s1_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            MessageBox.Show(e.PropertyName + " added");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
