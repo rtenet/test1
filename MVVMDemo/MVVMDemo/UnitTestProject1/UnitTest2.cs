ce6c31dd4da72c33fc97354a4eefb5e6b7240ad4
rtenet <rtenet@gmail.com>
2019-07-11 15:59:46 +0300

    Add unittests

---
+++ MVVMDemo/MVVMDemo/UnitTestProject1/UnitTest2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(2, 2);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(2, 2);
+        }
+    }
+}
