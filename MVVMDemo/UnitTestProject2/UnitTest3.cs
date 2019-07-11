589ee5f436f09c8301a997e22e23d1862bc465bc
rtenet <rtenet@gmail.com>
2019-07-11 17:02:42 +0300

    Add files via upload

---
+++ MVVMDemo/UnitTestProject2/UnitTest3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest3
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(2, 2);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(2, 3);
+        }
+    }
+}
